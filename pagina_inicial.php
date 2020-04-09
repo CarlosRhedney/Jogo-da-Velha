0c7cfd3cc5ee066318797218bb4254553e560706
Carlos Rhedney <twisterpsa@hotmail.com>
2020-04-09 00:31:00 -0300

    Add Update

---
+++ pagina_inicial.php
@@ -0,0 +1,17 @@
+<div id="pagina_inicial" align="center" style="background-color:#fff;">
+  <table border="0" style="width:600px;">
+    <thead>
+      <tr>
+        <td align="center" colspan="3">JOGO DA VELHA</td>
+      </tr>
+    </thead>
+    <tbody>
+      <!-- imagens dos jogadores na pagina inicial -->
+      <tr>
+        <td align="center"><img src="imagens/jogador_1.png" /><br /><input type="text" id="entrada_nome_jogador1" style="border:0px;" placeholder="Digite o nome do jogador 1" /></td>
+        <td align="center"><img src="imagens/iniciar.png" id="btn_inicia_jogo" /></td>
+        <td style="background-color:#fff;" align="center"><img src="imagens/jogador_2.png" /><br /><input type="text" id="entrada_nome_jogador2" style="border:0px;" placeholder="Digite o nome do jogador 2" /></td>
+      </tr>
+    </tbody>
+  </table>
+</div>
---
+++ pagina_jogo.php
@@ -0,0 +1,49 @@
+<div id="pagina_jogo" style="display:none; text-align:center; background-color:#fff;">
+  <!-- imagem do jogador 1 -->
+  <div style="display:inline-block;">
+    <img src="imagens/jogador_1.png" />
+    <br />
+    <span id="nome_jogador1"></span>
+  </div>      
+
+  <!-- Tabela com o grid -->
+  <div style="display:inline-block;">
+    <table border="0">
+      <tr>
+        <td style="border-bottom:1px solid red; border-right:1px solid red;">
+          <div class="jogada" id="a-1" style="width:90px; height:90px;"></div>
+        </td>
+        <td style="border-bottom:1px solid red; border-right:1px solid red;">
+          <div class="jogada" id="a-2" style="width:90px; height:90px;"></div>
+        </td>
+        <td style="border-bottom:1px solid red;">
+          <div class="jogada" id="a-3" style="width:90px; height:90px;"></div>
+        </td>
+      </tr>
+      <tr>
+        <td style="border-bottom:1px solid red; border-right:1px solid red;">
+          <div class="jogada" id="b-1" style="width:90px; height:90px;"></div>
+        </td>
+        <td style="border-bottom:1px solid red; border-right:1px solid red;">
+          <div class="jogada" id="b-2" style="width:90px; height:90px;"></div>
+        </td>
+        <td style="border-bottom:1px solid red;">
+          <div class="jogada" id="b-3" style="width:90px; height:90px;"></div>
+        </td>
+      </tr>
+      <tr>
+        <td style="border-right:1px solid red;">
+          <div class="jogada" id="c-1" style="width:90px; height:90px;"></div>
+        </td>
+        <td style="border-right:1px solid red;">
+          <div class="jogada" id="c-2" style="width:90px; height:90px;"></div>
+        </td>
+        <td>
+          <div class="jogada" id="c-3" style="width:90px; height:90px;"></div>
+        </td>
+      </tr>
+    </table>
+  </div>
+  <!-- imagem do jogador 2 -->
+  <div style="display:inline-block;"><img src="imagens/jogador_2.png" /><br /><span id="nome_jogador2"></span></div>
+</div>
